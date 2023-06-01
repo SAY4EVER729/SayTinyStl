706eace310e6785ab6048b017c5d14482078a58b
sayyy <shianyu999@163.com>
2023-06-01 16:42:22 +0800

    部分算法和vector容器的检查

---
+++ SayTinyStl/test/test.cpp
@@ -0,0 +1,34 @@
+#ifdef _MSC_VER
+#define _SCL_SECURE_NO_WARNINGS
+#endif
+
+
+#if defined(_MSC_VER) && defined(_DEBUG)
+#define _CRTDBG_MAP_ALLOC 
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif // check memory leaks
+
+#include "algorithm_performance_test.h"
+#include "algorithm_test.h"
+#include "../test/vector_test.h"
+
+
+int main()
+{
+	using namespace saystl::test;
+
+	std::cout.sync_with_stdio(false);
+
+	RUN_ALL_TESTS();
+	algorithm_performance_test::algorithm_performance_test();
+
+	//using namespace vector_test;
+	vector_say_test::vector_test();
+
+
+#if defined(_MSC_VER) && defined(_DEBUG)
+	_CrtDumpMemoryLeaks();
+#endif // check memory leaks
+
+}
